e72b5c278417c7760b9afc082478b98aa28e09cf
mdiaz26 <mike.diaz006@gmail.com>
2020-01-20 13:36:45 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    @kind = "triangle"
+  end
+
+  def kind
+    if @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+      raise TriangleError
+    elsif @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+      raise TriangleError
+    elsif @length1 == @length2 && @length2 == @length3
+      @kind = :equilateral
+    elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+      @kind = :isosceles
+    elsif @length1 != @length2 && @length2 != @length3
+      @kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a triangle"
+    end
+  end
 end
